03a63231ca4ae24628f4804164a35953590de671
MichalDz <info@admi.tech>
2023-11-28 10:21:27 +0100

    First Commit

---
+++ luhn.py
@@ -0,0 +1,20 @@
+###################################
+# !/usr/bin/python
+# Python 3.10
+# (C) 2023 admi.tech
+###################################
+
+# Import main packages
+
+
+# Main Functions
+def verify_card(data):
+
+    return data
+
+
+
+if __name__ == "__main__":
+    sample_card_no = "5610561081018250"
+
+    print(verify_card(sample_card_no))
